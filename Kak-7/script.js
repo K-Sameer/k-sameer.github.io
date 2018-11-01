69822496821aad9bcc0e8bd89fb6528c6f4db95f
K-Sameer <sulphate@icloud.com>
2018-11-01 23:19:30 +0200

    Добавил невидимые элементы описаний. Работа над кроссбраузерностью.

--- Kak-7/script.js
+++ Kak-7/script.js
@@ -14,26 +14,4 @@ var search = document.querySelector(".controls-box__search");
 toggleSearch.addEventListener('click', function(){
   search.classList.toggle("visible");
 });
-// слайдер
-var sliderSwitch = document.querySelectorAll(".slider__switch");
-
-var sliderSwitcher = document.querySelectorAll(".slider__switch .slider__switcher");
-
-for (var i = 0; i < sliderSwitcher.length; i++) {
-	sliderSwitcher[i].addEventListener('click', ((n) => { 
-
-    return function() {
-      var sliderSwitcherActive = document.querySelector(".slider__switcher--active");
-      var sliderItemVisited = document.querySelector(".slider__list-item--visited");
-      var sliderItem = document.querySelectorAll(".slider__list-item");
-
-      sliderSwitcherActive.classList.remove("slider__switcher--active");
-      this.classList.add("slider__switcher--active");
-
-      sliderItemVisited.classList.remove("slider__list-item--visited");
-      sliderItem[n].classList.add("slider__list-item--visited");
-    }
-
-  })(i))
-}
 
